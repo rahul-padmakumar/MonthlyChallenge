7ae3f2585bcbf1f8a3f055986d1bd16f47b8e3f5
rahulp <rahulp@qburst.com>
2025-01-10 18:27:26 +0530

    Use template inheritance

--- challenges/views.py
+++ challenges/views.py
@@ -34,13 +34,5 @@ def monthly_challenge(request, month):
     return render(request, "challenges/challenge.html", {"month_key": month_name,"month_code": month})
     
 def month_list(request):
-    data = list(map(_map_month, list(monthly_challenges_dict.keys())))
-    raw_response = "".join(data)
-    respone = f"<ul>{raw_response}</ul>"
-    return HttpResponse(respone)
-
-
-def _map_month(value): 
-    path = reverse("monthlychallenge", args=[value])
-    return_value = f"<li><a href={path}>{monthly_challenges_dict[value]}</a></li>"
-    return return_value
+    data = list(monthly_challenges_dict.keys())
+    return render(request, "challenges/index.html", {"months": data})
