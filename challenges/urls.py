5fba031d38764c3c11a8bcf454d94ddc18d7dab0
rahulp <rahulp@qburst.com>
2025-01-10 18:40:55 +0530

    Usage of template composition

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.month_list),
+    path("", views.month_list, name="index"),
     path("<int:month>", views.monthly_challenge_view_int),
     path("<str:month>", views.monthly_challenge, name='monthlychallenge')
 ]
